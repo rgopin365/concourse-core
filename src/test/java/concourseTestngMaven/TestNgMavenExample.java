2a01338a4fe8f9ede3bd79ebbcd4d161ad76e5b9
rgopin365 <126015645+rgopin365@users.noreply.github.com>
2023-11-15 12:16:19 +0530

    Add files via upload
    Added basic files for running maven+ sample testng code

---
+++ src/test/java/concourseTestngMaven/TestNgMavenExample.java
@@ -0,0 +1,11 @@
+package concourseTestngMaven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExample {
+	  @Test
+	    public void exampleOfTestNgMaven() {
+	        System.out.println("This is TestNG-Maven Example");
+	    }
+
+}
